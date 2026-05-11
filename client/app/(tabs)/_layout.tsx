44e9600ef848c8a6b1afa747ec1f85f8c3652aa9
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-05-11 16:46:32 +0200

    feat: :sparkles: implement tab navigation with Home, Scan, and History screens

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,5 +1,57 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "tamagui";
 
 export default function Layout() {
-    return <Tabs screenOptions={{ headerShown: false }} />;
+    const theme = useTheme();
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: theme.accent11.val,
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={24}
+                            name={focused ? "home" : "home-outline"}
+                            color={theme.background.val}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="scan"
+                options={{
+                    title: "Scan",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={24}
+                            name={focused ? "scan" : "scan-outline"}
+                            color={theme.accent3.val}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="history"
+                options={{
+                    title: "History",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={24}
+                            name={focused ? "time" : "time-outline"}
+                            color={theme.accent3.val}
+                        />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
