2bd562b88ab260ed79b6ec8f9ce4a1e99cfb1114
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-05-11 13:06:14 +0200

    chore: :heavy_plus_sign: update package.json to add Tamagui dependencies

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,24 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
+import { TamaguiProvider } from "tamagui";
+import { config } from "./tamagui.config";
 export const unstable_settings = {
-  anchor: '(tabs)',
+    anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return (
+        <TamaguiProvider config={config} defaultTheme={"dark"}>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen
+                    name="modal"
+                    options={{ presentation: "modal", title: "Modal" }}
+                />
+            </Stack>
+            <StatusBar style="auto" />
+        </TamaguiProvider>
+    );
 }
---
+++ client/app/tamagui.config.ts
@@ -0,0 +1,10 @@
+import {  createTamagui } from '@tamagui/core'
+import { defaultConfig } from '@tamagui/config/v5'
+
+export const config = createTamagui(defaultConfig)
+
+type Conf = typeof config
+
+declare module '@tamagui/core' {
+  interface TamaguiCustomConfig extends Conf {}
+}
