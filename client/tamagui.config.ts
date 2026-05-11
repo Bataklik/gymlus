7a2d9492a5e0e9666a4452659e7386bbff463ebe
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-05-11 21:36:45 +0200

    refactor: :art: update import path for createTamagui and improve formatting in tamagui.config.ts

--- client/tamagui.config.ts
+++ client/tamagui.config.ts
@@ -1,11 +1,16 @@
-import { createTamagui } from "@tamagui/core";
+import { createTamagui } from "tamagui";
 import { defaultConfig } from "@tamagui/config/v5";
 import { themes } from "./constants/theme";
 
-export const config = createTamagui({ ...defaultConfig, themes });
+export const config = createTamagui({
+  ...defaultConfig,
+  themes,
+});
 
 type Conf = typeof config;
 
-declare module "@tamagui/core" {
-    interface TamaguiCustomConfig extends Conf {}
+declare module "tamagui" {
+  interface TamaguiCustomConfig extends Conf {}
 }
+
+export default config;
