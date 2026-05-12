18add860026a6d179883ab7bb00085194412fc7a
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-05-12 17:51:50 +0200

    feat: :sparkles: add CardScan component for gym equipment identification

---
+++ client/components/home/card-scan.tsx
@@ -0,0 +1,57 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { YStack, XStack, Text, Button } from "tamagui";
+
+export default function CardScan() {
+    return (
+        <YStack
+            elevation={10}
+            backgroundColor="$accent11"
+            blockSize={300}
+            borderTopLeftRadius={40}
+            borderTopRightRadius={40}
+            borderBottomLeftRadius={40}
+            borderBottomRightRadius={40}
+            padding={35}
+        >
+            <XStack
+                paddingHorizontal={8}
+                paddingVertical={5}
+                marginBottom={10}
+                width={80}
+                justify={"center"}
+                backgroundColor={"$accent2"}
+                borderTopLeftRadius={10}
+                borderTopRightRadius={10}
+                borderBottomLeftRadius={10}
+                borderBottomRightRadius={10}
+            >
+                <Text fontWeight={"$40"} color={"$accent8"}>
+                    AI - SCAN
+                </Text>
+            </XStack>
+            <YStack gap={12}>
+                <YStack>
+                    <Text fontSize={30}>Point. Scan.</Text>
+                    <Text fontSize={30}>Know any machine.</Text>
+                </YStack>
+                <Text color="$color8" fontSize={18}>
+                    Identify gym equiment instantly and learn perfect form.
+                </Text>
+                <Button
+                    borderTopLeftRadius={30}
+                    borderTopRightRadius={30}
+                    borderBottomLeftRadius={30}
+                    borderBottomRightRadius={30}
+                    backgroundColor="$accent7"
+                    width={180}
+                >
+                    <XStack gap={8}>
+                        <Ionicons color="$color1" name="scan" size={18} />
+                        <Text color="$color1">Quick scan</Text>
+                    </XStack>
+                </Button>
+            </YStack>
+        </YStack>
+    );
+}
