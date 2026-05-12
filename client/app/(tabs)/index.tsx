2f6388baafced9ad8f0f179e5f0e2f2559654bda
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-05-12 17:52:05 +0200

    feat: :sparkles: enhance Home component layout with improved structure and spacing

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,10 +1,23 @@
+import CardScan from "@/components/home/card-scan";
+import Header from "@/components/layout/header";
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { Text, YStack } from "tamagui";
+import { YStack, Text, XStack, Button } from "tamagui";
 
 export default function Home() {
     return (
-        <YStack flex={1} backgroundColor="$accent12">
-            <Text>Home</Text>
+        <YStack
+            flex={1}
+            backgroundColor="$accent12"
+            gap={20}
+            paddingBlockStart={20}
+        >
+            {/* Header */}
+            <Header />
+            {/* Card scan */}
+            <CardScan />
+            {/* Card stats*/}
+            <XStack></XStack>
         </YStack>
     );
 }
