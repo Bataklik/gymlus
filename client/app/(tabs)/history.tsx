b4a9232f62442ad6c10c9f5f65fae89882ad8cab
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-22 16:15:54 +0200

    feat: :sparkles: implement search functionality and display history items in History screen

--- client/app/(tabs)/history.tsx
+++ client/app/(tabs)/history.tsx
@@ -1,52 +1,135 @@
+import { HistoryItem } from "@/components/history/history-item";
 import Header from "@/components/layout/header";
-import { Ionicons } from "@expo/vector-icons";
-import React from "react";
-import { Image, Input, ScrollView, XStack, YStack, Text } from "tamagui";
+import React, { useEffect, useState } from "react";
+import { FlatList as RNFlatList } from "react-native";
+import { Input, styled, YStack } from "tamagui";
 
+const FlatList = styled(RNFlatList, {
+    name: "TamaguiFlatList",
+    flex: 1,
+    marginBlockEnd: "$13",
+});
+
+interface HistoryItemData {
+    exerciseImage: string;
+    exerciseName: string;
+    muscleGroup: string;
+    time: string;
+}
 export default function History() {
+    let [historyData, setHistoryData] = useState<HistoryItemData[]>([
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Cable Lat Pulldown",
+            muscleGroup: "Back",
+            time: "8:42 AM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Bench Press",
+            muscleGroup: "Chest",
+            time: "9:15 AM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Seated Row",
+            muscleGroup: "Back",
+            time: "10:42 AM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Shoulder Press",
+            muscleGroup: "Shoulders",
+            time: "11:30 AM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Bicep Curl",
+            muscleGroup: "Arms",
+            time: "12:00 PM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Tricep Extension",
+            muscleGroup: "Arms",
+            time: "1:15 PM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Squat",
+            muscleGroup: "Legs",
+            time: "2:30 PM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Lunges",
+            muscleGroup: "Legs",
+            time: "3:45 PM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Plank",
+            muscleGroup: "Core",
+            time: "4:00 PM",
+        },
+        {
+            exerciseImage: "https://placehold.co/75.jpg",
+            exerciseName: "Push-ups",
+            muscleGroup: "Chest",
+            time: "5:15 PM",
+        },
+    ]);
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [historyList, setHistoryList] = useState(historyData);
+    const searchHandler = (text: string) => {
+        setHistoryList((prevData) =>
+            prevData.filter((item) =>
+                item.exerciseName.toLowerCase().includes(text.toLowerCase()),
+            ),
+        );
+    };
+    useEffect(() => {
+        if (searchTerm === "") {
+            setHistoryList(historyData);
+        } else {
+            searchHandler(searchTerm);
+        }
+    }, [historyData, searchTerm]);
+
     return (
-        <YStack flex={1} bg="$accent12" gap={20} paddingBlockStart={20}>
+        <YStack flex={1} bg="$accent12" gap={"$3"} paddingBlockStart={"$4"}>
             <Header
                 title="History"
                 onSettingsPress={() => console.log("Settings pressed")}
             />
-            <YStack gap={15} paddingInline={8}>
+            <YStack flex={1} gap={"$3"} paddingInline={"$3"}>
                 <Input
                     size="$4"
                     placeholder={"Search history..."}
                     placeholderTextColor={"$color10"}
-                    borderWidth={2}
+                    borderWidth={"$1"}
+                    onChange={(text) => setSearchTerm(text.target.value)}
                 />
 
-                <ScrollView>
-                    <XStack justify={"space-between"} paddingBlock={10}>
-                        <XStack gap={10}>
-                            <Image
-                                src={"https://placehold.co/75.jpg"}
-                                style={{
-                                    width: 75,
-                                    height: 75,
-                                    borderRadius: 10,
-                                }}
+                <FlatList
+                    data={historyList}
+                    keyExtractor={(item) => {
+                        const historyItem = item as (typeof historyData)[0];
+                        return `${historyItem.exerciseName}-${historyItem.time}`;
+                    }}
+                    showsVerticalScrollIndicator={true}
+                    renderItem={({ item }) => {
+                        const historyItem = item as (typeof historyData)[0];
+                        return (
+                            <HistoryItem
+                                exerciseImage={historyItem.exerciseImage}
+                                exerciseName={historyItem.exerciseName}
+                                muscleGroup={historyItem.muscleGroup}
+                                time={historyItem.time}
                             />
-                            <YStack>
-                                <Text fontWeight={800} fontSize={18}>
-                                    Cable Lat Pulldown
-                                </Text>
-                                <Text fontSize={14} color={"$color11"}>
-                                    Back - 8:42 AM
-                                </Text>
-                            </YStack>
-                        </XStack>
-                        <YStack justify={"center"}>
-                            <Ionicons
-                                name="chevron-forward"
-                                size={18}
-                                color={"white"}
-                            />
-                        </YStack>
-                    </XStack>
-                </ScrollView>
+                        );
+                    }}
+                />
             </YStack>
         </YStack>
     );
