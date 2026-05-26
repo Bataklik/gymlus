81d62c44eb07d9a45f180a9cb832e954d0cdbda8
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-26 17:53:38 +0200

    refactor: :recycle: refactor Scan component by removing unused image state and updating background color property

--- client/app/scan.tsx
+++ client/app/scan.tsx
@@ -4,31 +4,25 @@ import { CameraView } from "expo-camera";
 import ScanHeader from "@/components/scan/scan-header";
 import { useScanCamera } from "@/hooks/useScanCamera";
 import ScanBotbar from "@/components/scan/scan-botbar";
-import { router } from "expo-router";
-
+// https://ra-6446.medium.com/send-an-image-to-your-backend-with-the-fetch-function-javascript-react-9134f9935eb
+// https://dev.to/sjamescarter/uploading-images-in-react-3lmb
+// https://www.youtube.com/watch?v=QluwtZZedek
 export default function Scan() {
     const {
         cameraPermission,
         requestCameraPermission,
         isFlashOn,
         camera,
-        image,
-        setImage,
         takePicture,
         closeHandler,
         flashHandler,
         pickImage,
         requestMediaLibraryPermission,
     } = useScanCamera();
+
     useEffect(() => {
         requestMediaLibraryPermission();
     }, []);
-
-    useEffect(() => {
-        if (!image) return;
-        router.push({ pathname: "/exercise", params: { image } });
-        setImage(null);
-    }, [image, setImage]);
 
     if (!cameraPermission) {
         // Camera permissions are still loading.
@@ -39,7 +33,7 @@ export default function Scan() {
         return (
             <YStack
                 flex={1}
-                backgroundColor="$accent12"
+                bg="$accent12"
                 paddingBlockStart={40}
                 elevation={10}
                 gap={20}
@@ -60,7 +54,7 @@ export default function Scan() {
     return (
         <YStack
             flex={1}
-            backgroundColor="$accent12"
+            bg="$accent12"
             paddingBlockStart={40}
             elevation={10}
             gap={20}
