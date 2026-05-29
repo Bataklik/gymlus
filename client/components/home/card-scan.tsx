702aee30f213f1225b3fb323fa5e4b9462203524
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-29 16:30:50 +0200

    style: :lipstick: update Dutch translations and optimize component styling in CardScan

--- client/components/home/card-scan.tsx
+++ client/components/home/card-scan.tsx
@@ -1,3 +1,4 @@
+import { router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { YStack, XStack, Text, Button } from "tamagui";
@@ -6,21 +7,21 @@ export default function CardScan() {
     return (
         <YStack
             elevation={10}
-            backgroundColor="$accent11"
+            bg="$accent11"
             blockSize={300}
             borderTopLeftRadius={20}
             borderTopRightRadius={20}
             borderBottomLeftRadius={20}
             borderBottomRightRadius={20}
-            padding={35}
+            p={35}
         >
             <XStack
-                paddingHorizontal={8}
-                paddingVertical={5}
-                marginBottom={10}
+                paddingBlock={8}
+                paddingInline={5}
+                marginBlockEnd={10}
                 width={80}
                 justify={"center"}
-                backgroundColor={"$accent2"}
+                bg={"$accent2"}
                 borderTopLeftRadius={10}
                 borderTopRightRadius={10}
                 borderBottomLeftRadius={10}
@@ -32,24 +33,26 @@ export default function CardScan() {
             </XStack>
             <YStack gap={12}>
                 <YStack>
-                    <Text fontSize={30}>Point. Scan.</Text>
-                    <Text fontSize={30}>Know any machine.</Text>
+                    <Text fontSize={30}>Richt. Scan.</Text>
+                    <Text fontSize={30}>Ken elke machine.</Text>
                 </YStack>
                 <Text color="$color8" fontSize={18}>
-                    Identify gym equiment instantly and learn perfect form.
+                    Identificeer direct fitnessapparaten en leer de juiste
+                    techniek.
                 </Text>
                 <Button
                     borderTopLeftRadius={30}
                     borderTopRightRadius={30}
                     borderBottomLeftRadius={30}
                     borderBottomRightRadius={30}
-                    backgroundColor="$accent7"
+                    bg="$accent7"
                     width={160}
+                    onPress={() => router.push("/scan")}
                 >
                     <XStack gap={8} justify={"center"}>
                         <Ionicons color="$color1" name="scan" size={18} />
                         <Text color="$color1" fontWeight={700}>
-                            Quick scan
+                            Snelle scan
                         </Text>
                     </XStack>
                 </Button>
