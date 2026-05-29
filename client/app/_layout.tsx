98ef448ebd3d67f9009d81e11b1fba1b90c83809
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-29 17:44:32 +0200

    feat: :zap: wrap RootLayout in Redux Provider for state management

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -4,32 +4,39 @@ import "react-native-reanimated";
 
 import { TamaguiProvider } from "tamagui";
 import { config } from "../tamagui.config";
+import { Provider } from "react-redux";
+import { store } from "./store";
 export const unstable_settings = {
     anchor: "(tabs)",
 };
 
 export default function RootLayout() {
     return (
-        <TamaguiProvider config={config} defaultTheme={"dark"}>
-            <Stack
-                screenOptions={{
-                    headerShown: false,
-                }}
-            >
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen
-                    name="scan"
-                    options={{
-                        presentation: "fullScreenModal",
-                        animation: "slide_from_bottom",
+        <Provider store={store}>
+            <TamaguiProvider config={config} defaultTheme={"dark"}>
+                <Stack
+                    screenOptions={{
+                        headerShown: false,
                     }}
-                />
-                <Stack.Screen
-                    name="exercise_detail"
-                    options={{ animation: "slide_from_right" }}
-                />
-            </Stack>
-            <StatusBar style="auto" />
-        </TamaguiProvider>
+                >
+                    <Stack.Screen
+                        name="(tabs)"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="scan"
+                        options={{
+                            presentation: "fullScreenModal",
+                            animation: "slide_from_bottom",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="exercise_detail"
+                        options={{ animation: "slide_from_right" }}
+                    />
+                </Stack>
+                <StatusBar style="auto" />
+            </TamaguiProvider>
+        </Provider>
     );
 }
