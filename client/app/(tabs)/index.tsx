fb3a51bc5a7cd192e86bbd568da8ed474b8dcc1b
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-31 15:35:51 +0200

    feat: :sparkles: update Header component to navigate to favorites page

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -4,17 +4,18 @@ import RecentlyScanned from "@/components/home/recently-scanned";
 import Header from "@/components/layout/header";
 import React, { useState } from "react";
 import { YStack } from "tamagui";
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
+import { router } from "expo-router";
 export default function Home() {
     const [dayStreak, setDayStreak] = useState(4);
     const [scans, setScans] = useState(21);
     const [saved, setSaved] = useState(67);
-    const favorites = useSelector((state: RootState) => state.favorites.items);
     return (
         <YStack flex={1} bg="$accent12" gap={20} paddingBlockStart={20}>
             {/* Header */}
-            <Header title="Home" onButtonPress={() => console.log(favorites)} />
+            <Header
+                title="Home"
+                onButtonPress={() => router.push("/favorites")}
+            />
             <YStack gap={15} paddingInline={8}>
                 {/* Card scan */}
                 <CardScan />
