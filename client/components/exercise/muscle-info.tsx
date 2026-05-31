d1dc19bd082b39280f6da286f03fe5609c7d71e6
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-31 13:16:58 +0200

    feat: :refactor: update MuscleInfo component to use Muscle type and simplify muscle array handling

--- client/components/exercise/muscle-info.tsx
+++ client/components/exercise/muscle-info.tsx
@@ -1,17 +1,19 @@
 import { YStack, XStack, Text } from "tamagui";
 import { MuscleItem } from "./muscle-item";
+import { Muscle } from "@/types";
 
 interface MuscleInfoProps {
-    muscles: { name: string; main: boolean }[];
+    muscles: Muscle[];
 }
 export function MuscleInfo({ muscles }: MuscleInfoProps) {
+    const muscleArray = Object.values(muscles);
     return (
         <YStack>
             <Text fontSize={18} fontWeight={600}>
                 Spiergroepen
             </Text>
             <XStack gap={10} mt={10} flexWrap="wrap" justify={"flex-start"}>
-                {muscles.map((muscle, index) => (
+                {muscleArray.map((muscle, index) => (
                     <MuscleItem
                         key={index}
                         name={muscle.name}
